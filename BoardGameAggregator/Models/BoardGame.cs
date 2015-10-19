2775b9a630a996bd09ffc1d1393e5f4997001090
Tracy Moody <tracyangelamoody@gmail.com>
2015-10-19 15:07:10 -0500

    Add basic crud functionality for a board game.

---
+++ BoardGameAggregator/Models/BoardGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BoardGameAggregator.Models
+{
+    public class BoardGame
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Played { get; set; }
+        
+        public bool Owned { get; set;  }
+
+        public string Comments { get; set; }
+
+    }
+}
